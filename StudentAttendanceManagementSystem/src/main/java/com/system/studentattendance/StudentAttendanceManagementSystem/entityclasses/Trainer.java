794724c0129b5ac789c22b7349f713ab88e58540
Ajayp <ajyp1404@gmail.com>
2025-06-28 18:00:07 +0530

    first comment

--- StudentAttendanceManagementSystem/src/main/java/com/system/studentattendance/StudentAttendanceManagementSystem/entityclasses/Trainer.java
+++ StudentAttendanceManagementSystem/src/main/java/com/system/studentattendance/StudentAttendanceManagementSystem/entityclasses/Trainer.java
@@ -3,4 +3,34 @@ package com.system.studentattendance.StudentAttendanceManagementSystem.entitycla
 public class Trainer {
 
 	private int tid;
+	private String name;
+	private String phno;
+	private String email;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 }
